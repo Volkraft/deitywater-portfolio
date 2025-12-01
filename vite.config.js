5f86424f886de8d9c3174af534006a6b14070308
Denis <volkraft9@gmail.com>
2025-12-01 17:21:38 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [
-    react({
-      include: /\.[jt]sx?$/,
-    }),
-  ],
-  base: './',
+  plugins: [react({ include: /\.[jt]sx?$/ })],
+  base: '/deitywater-portfolio/',   // имя репозитория
+  build: {
+    outDir: 'docs',
+  },
 })
